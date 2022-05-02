4c0a18d73f43f7aad4ffc2ce1b15c8a6445e204a
Halil Gokcedag <halilhs20@gmail.com>
2022-05-02 20:58:54 +0300

    variables instance count to 3

--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "keypair" {
     default = "demokey.pub"
   
 }
-# variable "keypair-private" {
-#     default = "demokey"
-  
-# }
 
 variable "instance_count" {
     description = "Number of the EC2 instances: 1 Controller, 2 workers"
